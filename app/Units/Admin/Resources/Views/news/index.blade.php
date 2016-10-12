9d5ce979ba2839a633b75dd1196de2e69f2bf96f
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2016-10-12 15:52:20 -0300

    Implementação de Funcionalidade - Cadastrar Noticias

--- app/Units/Admin/Resources/Views/news/index.blade.php
+++ app/Units/Admin/Resources/Views/news/index.blade.php
@@ -17,51 +17,7 @@
 
             </thead>
             <tbody>
-            @foreach($news as $row)@extends('admin::layout')
-
-@section('content')
-    <h3>Notícias</h3>
-    <div class="col-md-8 col-md-offset-2">
-        <table class="table">
-
-            <thead>
-            <tr>
-                <th>Titulo</th>
-                <th>Url</th>
-                <th>Data</th>
-                <th>#</th>
-                <th>#</th>
-            </tr>
-
-            </thead>
-            <tbody>
             @foreach($news as $row)
-                <tr>
-                    <td>{{$row->title}}</td>
-                    <td><a href="{{$row->url}}" target="_blank">Link</a></td>
-                    <td>{{$row->created_at}}</td>
-                    <td>
-                        <span class="btn btn-xs btn-success">Ativado</span>
-                    </td>
-                    <td>
-                        <a href="{{route('admin.news.edit', $row->id)}}">
-                            <span class="glyphicon glyphicon-edit btn btn-xs btn-success"></span>
-                        </a>
-
-
-                    </td>
-                    <td>
-                        <a href="{{route('admin.news.edit', $row->id)}}">
-                            <span class="glyphicon glyphicon-trash btn btn-xs btn-danger"></span>
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-    </div>
-
-@endsection
                 <tr>
                     <td>{{$row->title}}</td>
                     <td><a href="{{$row->url}}" target="_blank">Link</a></td>
