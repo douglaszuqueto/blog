c3fc89f5ad3e312cd892be00e385750bef2b55da
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-12 19:49:34 -0300

    Seeder -> Contact

--- app/Domains/Contact/Database/Seeders/ContactSeeder.php
+++ app/Domains/Contact/Database/Seeders/ContactSeeder.php
@@ -2,7 +2,7 @@
 
 namespace App\Domains\Contact\Database\Seeders;
 
-use App\Domains\Contacts\Entities\Contact;
+use App\Domains\Contact\Entities\Contact;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -16,11 +16,12 @@ class ContactSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('Contact')->insert([
-            'id' => 1,
-            'name' => 'Douglas Zuqueto',
-            'email' => 'douglas.zuqueto@gmail.com',
-            'password' => bcrypt('admin'),
+        DB::table('contact')->insert([
+            'title' => 'News 1',
+            'subject' => 'Subject 1',
+            'message' => 'Message 1',
+            'email' => 'email@mail.com',
+            'state' => 0,
         ]);
 
 //        factory(Contact::class)->create();
