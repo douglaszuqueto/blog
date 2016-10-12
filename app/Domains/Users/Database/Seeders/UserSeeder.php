845d84e2a54ed96b9de806ea368126db6caa7285
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-12 19:56:02 -0300

    Seeder -> User

--- app/Domains/Users/Database/Seeders/UserSeeder.php
+++ app/Domains/Users/Database/Seeders/UserSeeder.php
@@ -17,7 +17,6 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'id' => 1,
             'name' => 'Douglas Zuqueto',
             'email' => 'douglas.zuqueto@gmail.com',
             'password' => bcrypt('admin'),
