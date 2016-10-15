0698cbd0be913a8d23e0589d3b9395185546969c
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2016-10-15 16:26:49 -0300

    Layout - Header

--- app/Units/Admin/Resources/Views/_header/header.blade.php
+++ app/Units/Admin/Resources/Views/_header/header.blade.php
@@ -1,9 +1,19 @@
-<style>header, main, footer {
+<style>
+    main, footer {
         padding-left: 240px;
+        padding-top: 65px;
+    }
+    header{position: absolute; width: 100%; z-index: 9999;}
+
+    #nav-mobile{
+        margin-top: 65px;
     }
 
+    #nav-title{
+        margin: -5px 0 0 10px;
+    }
     @media only screen and (max-width: 992px) {
-        header, main, footer {
+        main, footer {
             padding-left: 0;
         }
 
@@ -27,114 +37,116 @@
         background-color: #008BD3;
     }</style>
 <header>
-    <nav class="top-nav">
-        <div class="">
-            <a href="#" data-activates="nav-mobile" class="button-collapse top-nav full hide-on-large-only">
-                <i class="material-icons">menu</i>
-            </a>
-            <div class="nav-wrapper">
-                <ul class="right">
-                    <li>
-                        <a href="{{route('auth.logout')}}">Sair<i class="material-icons right">input</i></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <ul id="nav-mobile" class="side-nav fixed">
-        <br>
-        <h4 class=" center" style="color: #009BC9;">@douglaszuqueto</h4>
-        <br>
-        <div class="divider"></div>
-        <li class="bold">
-            <a href="{{route('admin.dashboard.index')}}" class="waves-effect waves-blue">Dashboard</a>
-        </li>
-        <li class="no-padding">
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold">
-                    <a class="waves-effect waves-blue">Estatísticas</a>
-                </li>
-            </ul>
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold">
-                    <a class="collapsible-header  waves-effect waves-blue">Artigos</a>
-                    <div class="collapsible-body">
-                        <ul>
-                            <li><a href="{{route('admin.articles.index')}}">Artigos</a></li>
-                            <li><a href="">Cadastrar Artigo</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold">
-                    <a class="collapsible-header  waves-effect waves-blue">Usuários</a>
-                    <div class="collapsible-body">
-                        <ul>
-                            <li><a href="{{route('admin.users.index')}}">Usuários</a></li>
-                            <li><a href="{{route('admin.users.create')}}">Cadastrar Usuário</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold">
-                    <a class="collapsible-header  waves-effect waves-blue">Notícias</a>
-                    <div class="collapsible-body">
-                        <ul>
-                            <li><a href="{{route('admin.news.index')}}">Notícias</a></li>
-                            <li><a href="{{route('admin.news.create')}}">Cadastrar Notícia</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold">
-                    <a class="collapsible-header  waves-effect waves-blue">Mensagens</a>
-                    <div class="collapsible-body">
-                        <ul>
-                            <li><a href="{{route('admin.contact.index')}}">Mensagens</a></li>
-                            <li><a href="">Cadastrar Mensagens</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold">
-                    <a class="collapsible-header  waves-effect waves-blue">Patrocinadores</a>
-                    <div class="collapsible-body">
-                        <ul>
-                            <li><a href="{{route('admin.sponsors.index')}}">Patrocinadores</a></li>
-                            <li><a href="{{route('admin.sponsors.create')}}">Cadastrar Patrocinador</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold">
-                    <a class="collapsible-header  waves-effect waves-blue">Apoiadores</a>
-                    <div class="collapsible-body">
-                        <ul>
-                            <li><a href="{{route('admin.supporters.index')}}">Apoiadores</a></li>
-                            <li><a href="{{route('admin.supporters.create')}}">Cadastrar Apoiador</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
+    <div class="navbar-fixed">
 
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold">
-                    <a class="collapsible-header  waves-effect waves-blue">Configurações</a>
-                    <div class="collapsible-body">
-                        <ul>
-                            <li><a href="">Configurações</a></li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </li>
-    </ul>
+        <nav>
+            <div class="">
+                <a href="#" data-activates="nav-mobile" class="button-collapse top-nav full hide-on-large-only">
+                    <i class="material-icons">menu</i>
+                </a>
+                <div class="nav-wrapper">
+                    <ul id="nav-title" class="left">
+                        <h4 class="">@douglaszuqueto</h4>
+                    </ul>
+                    <ul class="right">
+                        <li>
+                            <a href="{{route('auth.logout')}}">Sair<i class="material-icons right">input</i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
 </header>
+<ul id="nav-mobile" class="side-nav fixed">
+    <li class="bold">
+        <a href="{{route('admin.dashboard.index')}}" class="waves-effect waves-blue">Dashboard</a>
+    </li>
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="waves-effect waves-blue">Estatísticas</a>
+            </li>
+        </ul>
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="collapsible-header  waves-effect waves-blue">Artigos</a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="{{route('admin.articles.index')}}">Artigos</a></li>
+                        <li><a href="">Cadastrar Artigo</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="collapsible-header  waves-effect waves-blue">Usuários</a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="{{route('admin.users.index')}}">Usuários</a></li>
+                        <li><a href="{{route('admin.users.create')}}">Cadastrar Usuário</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="collapsible-header  waves-effect waves-blue">Notícias</a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="{{route('admin.news.index')}}">Notícias</a></li>
+                        <li><a href="{{route('admin.news.create')}}">Cadastrar Notícia</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="collapsible-header  waves-effect waves-blue">Mensagens</a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="{{route('admin.contact.index')}}">Mensagens</a></li>
+                        <li><a href="">Cadastrar Mensagens</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="collapsible-header  waves-effect waves-blue">Patrocinadores</a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="{{route('admin.sponsors.index')}}">Patrocinadores</a></li>
+                        <li><a href="{{route('admin.sponsors.create')}}">Cadastrar Patrocinador</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="collapsible-header  waves-effect waves-blue">Apoiadores</a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="{{route('admin.supporters.index')}}">Apoiadores</a></li>
+                        <li><a href="{{route('admin.supporters.create')}}">Cadastrar Apoiador</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+
+        <ul class="collapsible collapsible-accordion">
+            <li class="bold">
+                <a class="collapsible-header  waves-effect waves-blue">Configurações</a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="">Configurações</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+</ul>
 <script>
     $(document).ready(function ($) {
         $('.button-collapse').sideNav({'edge': 'left'});
