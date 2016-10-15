c644f2960ad296f5a49e17dec7df47a5cf9b5a8c
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-15 20:47:36 -0300

    Implementação - Estatisticas

--- app/Units/Admin/Resources/Views/statistics/index.blade.php
+++ app/Units/Admin/Resources/Views/statistics/index.blade.php
@@ -11,29 +11,6 @@
         <div class="row">
             <div class="col s12 m4 l4">
                 <div class="card-panel ">
-                    <h6 class="flow-text center-align">Art. Publicados</h6>
-                    <h4 class="center-align">12</h4>
-                </div>
-            </div>
-
-            <div class="col s12 m4 l4">
-                <div class="card-panel ">
-                    <h6 class="flow-text center-align">Art. Pendentes</h6>
-                    <h4 class="center-align">12</h4>
-                </div>
-            </div>
-
-            <div class="col s12 m4 l4">
-                <div class="card-panel ">
-                    <h6 class="flow-text center-align">Art. Agendados</h6>
-                    <h4 class="center-align">12</h4>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col s12 m4 l4">
-                <div class="card-panel ">
                     <p class="center-align" style="margin-top: -20px">
                         <a href="{{route('admin.contact.index')}}"><i class="material-icons medium green-text" title="Caixa de Entrada">email</i></a>
                     </p>
