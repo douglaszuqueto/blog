629104c2941b1f801aee5ebde4f102bcff2de8cf
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-17 19:59:09 -0200

    Layout - User Index

--- app/Units/Admin/Resources/Views/users/index.blade.php
+++ app/Units/Admin/Resources/Views/users/index.blade.php
@@ -12,8 +12,9 @@
                     <tr>
                         <th>Usuário</th>
                         <th>Email</th>
-                        <th>Status</th>
-                        <th>#</th>
+                        <th width="10%" class="center-align">Status</th>
+                        <th width="5%" class="center-align">#</th>
+                        <th width="5%" class="center-align">#</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -21,12 +22,17 @@
                         <tr>
                             <td>{{$row->name}}</td>
                             <td>{{$row->email}}</td>
-                            <td>
+                            <td class="center-align">
                                 <span class="">Ativado</span>
                             </td>
-                            <td>
+                            <td class="center-align">
                                 <a href="{{route('admin.users.edit', $row->id)}}">
                                     <i class="material-icons">mode_edit</i>
+                                </a>
+                            </td>
+                            <td class="center-align">
+                                <a href="{{route('admin.users.edit', $row->id)}}">
+                                    <i class="material-icons red-text">delete</i>
                                 </a>
                             </td>
                         </tr>
