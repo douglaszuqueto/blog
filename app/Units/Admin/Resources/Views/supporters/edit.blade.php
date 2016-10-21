15076307e012f361b8d74a4bc4e0fac4eedcbf63
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-21 21:08:44 -0200

    Update Status - Apoiador

--- app/Units/Admin/Resources/Views/supporters/edit.blade.php
+++ app/Units/Admin/Resources/Views/supporters/edit.blade.php
@@ -57,6 +57,14 @@
 
                     </div>
                 </div>
+                <div class="row">
+                    <div class="">
+                        <div class="col s12 l12">
+                            <input type="checkbox" id="state" name="state" {{$item->state ? 'checked' : ''}}/>
+                            <label for="state">Status</label>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="row">
                     <div class="input-field col s12 l12 ">
