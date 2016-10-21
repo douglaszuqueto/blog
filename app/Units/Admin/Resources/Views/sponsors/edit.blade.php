1d90c4d1ea9602a71fa31c2205e3e11612954a77
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-21 21:09:33 -0200

    Update Status - Patrocinador

--- app/Units/Admin/Resources/Views/sponsors/edit.blade.php
+++ app/Units/Admin/Resources/Views/sponsors/edit.blade.php
@@ -59,6 +59,15 @@
                 </div>
 
                 <div class="row">
+                    <div class="">
+                        <div class="col s12 l12">
+                            <input type="checkbox" id="state" name="state" {{$item->state ? 'checked' : ''}}/>
+                            <label for="state">Status</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
                     <div class="input-field col s12 l12 ">
                         <button type="submit" class="waves-effect waves-light btn right">
                             <i class="material-icons right">cloud</i>Atualizar
