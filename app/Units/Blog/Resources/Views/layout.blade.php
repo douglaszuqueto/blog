c0835c86aaabfffe283f0eb564c87ebaa064761c
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-23 17:42:25 -0200

    Teste de SEO

--- app/Units/Blog/Resources/Views/layout.blade.php
+++ app/Units/Blog/Resources/Views/layout.blade.php
@@ -4,13 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <meta name="description" content="douglaszuqueto">
     <meta name="apple-mobile-web-app-title" content="douglaszuqueto">
     <meta name="application-name" content="douglaszuqueto">
     <meta name="theme-color" content="#008BD3">
 
 
     {!! app('seotools')->generate() !!}
+
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <link rel="stylesheet" href="{{asset('css/materialize.min.css')}}">
