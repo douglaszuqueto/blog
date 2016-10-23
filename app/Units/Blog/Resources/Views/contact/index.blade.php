87b3178d493f89ebd63454024b159ecbd5bd298d
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-23 16:24:13 -0200

    Layout - Contato

--- app/Units/Blog/Resources/Views/contact/index.blade.php
+++ app/Units/Blog/Resources/Views/contact/index.blade.php
@@ -9,14 +9,14 @@
             <div class="col s12 l12">
                 <h5>Formulário de Contato</h5>
 
-                <form class="col s8" method="POST" action="{{route('blog.contact.send')}}">
+                <form class="col s12 m10 l8" method="POST" action="{{route('blog.contact.send')}}">
                     <input type="hidden" name="_token" value="{{csrf_token()}}">
                     <div class="row">
-                        <div class="input-field col s6">
+                        <div class="input-field col s12 m6 l6">
                             <input id="icon_prefix" type="text" name="name" class="validate">
                             <label for="icon_prefix">Nome</label>
                         </div>
-                        <div class="input-field col s6">
+                        <div class="input-field col s12 m6 l6">
                             <input id="icon_telephone" type="email" name="email" class="validate">
                             <label for="icon_telephone">Email</label>
                         </div>
