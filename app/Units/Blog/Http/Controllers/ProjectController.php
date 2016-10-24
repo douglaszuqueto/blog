7628dd1dfe9d6770e2251dc5f0a96a4d2d6d7b50
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-24 21:28:44 -0200

    Layout - Blog

--- app/Units/Blog/Http/Controllers/ProjectController.php
+++ app/Units/Blog/Http/Controllers/ProjectController.php
@@ -3,12 +3,24 @@
 namespace App\Units\Blog\Http\Controllers;
 
 use App\Support\Http\Controllers\Controller;
+use Artesaos\SEOTools\Facades\OpenGraph;
+use Artesaos\SEOTools\Facades\SEOMeta;
 
 class ProjectController extends Controller
 {
 
     public function project()
     {
+        SEOMeta::setTitle('O Projeto');
+        SEOMeta::setDescription('Venha conhecer o projeto IoTBr');
+        SEOMeta::setCanonical('https://douglaszuqueto.com/o-projeto');
+
+        OpenGraph::setTitle('O Projeto');
+        OpenGraph::setDescription('Venha conhecer o projeto IoTBr');
+        OpenGraph::setUrl('hhttps://douglaszuqueto.com/o-projeto');
+        OpenGraph::addProperty('type', 'website');
+        OpenGraph::addImage('https://douglaszuqueto.com/images/IoT.jpg');
+
         return $this->view('blog::project.index');
     }
 }
