16aea06bc02fe3707dc93c0d749babd80245cb8c
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-24 21:44:02 -0200

    Layout - Blog

--- app/Units/Blog/Resources/Views/contact/index.blade.php
+++ app/Units/Blog/Resources/Views/contact/index.blade.php
@@ -10,24 +10,24 @@
                     <input type="hidden" name="_token" value="{{csrf_token()}}">
                     <div class="row">
                         <div class="input-field col s12 m6 l6">
-                            <input id="icon_prefix" type="text" name="name" class="validate">
-                            <label for="icon_prefix">Nome</label>
+                            <input id="name" type="text" name="name" class="validate" required>
+                            <label for="name">Nome</label>
                         </div>
                         <div class="input-field col s12 m6 l6">
-                            <input id="icon_telephone" type="email" name="email" class="validate">
-                            <label for="icon_telephone">Email</label>
+                            <input id="email" type="email" name="email" class="validate" required>
+                            <label for="email">Email</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12">
-                            <input id="icon_prefix" type="text" name="subject" class="validate">
-                            <label for="icon_prefix">Assunto</label>
+                            <input id="subject" type="text" name="subject" class="validate" required>
+                            <label for="subject">Assunto</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12">
-                            <textarea id="textarea1" name="message" class="materialize-textarea"></textarea>
-                            <label for="textarea1">Mensagem</label>
+                            <textarea id="message" name="message" class="materialize-textarea" required></textarea>
+                            <label for="message">Mensagem</label>
                         </div>
                     </div>
                     <div class="row">
