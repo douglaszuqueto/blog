77d209a0d231a91c810f899493cb07db83ca3ba0
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-26 19:55:53 -0200

    Layout

--- app/Units/Blog/Resources/Views/_includes/header.blade.php
+++ app/Units/Blog/Resources/Views/_includes/header.blade.php
@@ -1,16 +1,34 @@
-<nav>
-    <div class="nav-wrapper">
-        <a href="{{route('blog.index')}}" class="brand-logo left">douglaszuqueto</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="{{route('blog.index')}}" class="menu">HOME</a></li>
-            <li><a href="{{route('blog.articles.index')}}" class="menu">ARTIGOS</a></li>
-            <li><a href="{{route('blog.project.index')}}" class="menu">O PROJETO</a></li>
-            <li><a href="{{route('blog.news.index')}}" class="menu">NOTÍCIAS</a></li>
-            <li><a href="{{route('blog.about-me.index')}}" class="menu">SOBRE MIM</a></li>
-            <li><a href="{{route('blog.contact.index')}}" class="menu">CONTATO</a></li>
-        </ul>
+<div class="">
+    <nav>
+        <div class="nav-wrapper container">
+            <div class="nav-left">
+                <a href="{{route('blog.index')}}" class="left title">
+                    <img src="{{asset('images/logo.png')}}" alt="" width="10%" style="padding-top: 5px">
+                    <span class=""></span>Douglas Zuqueto
+                </a>
+            </div>
+
+            <div class="nav-menu">
+                <ul id="nav-mobile" class=" hide-on-med-and-down">
+                    <li><a href="{{route('blog.index')}}" class="menu">Home</a></li>
+                    <li><a href="{{route('blog.articles.index')}}" class="menu">Artigos</a></li>
+                    <li><a href="{{route('blog.project.index')}}" class="menu">O Projeto</a></li>
+                    <li><a href="{{route('blog.news.index')}}" class="menu">Notícias</a></li>
+                    <li><a href="{{route('blog.about-me.index')}}" class="menu">Sobre Mim</a></li>
+                    <li><a href="{{route('blog.contact.index')}}" class="menu">Contato</a></li>
+                </ul>
+            </div>
+
+            <div class="nav-right">
+                <ul>
+                    <li><a href=""><span class="material-icons">info_outline</span></a></li>
+                    <li><a href=""><span class="material-icons">info_outline</span></a></li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+    <div class="l12 hide-on-med-and-down center" style="height: 85px">
+        {{--    <img src="{{asset('images/esp8266.jpg')}}" alt="" class="responsive-img" style="height: 300px">--}}
     </div>
-</nav>
-<div class="l12 hide-on-med-and-down center" style="height: 85px">
-    {{--    <img src="{{asset('images/esp8266.jpg')}}" alt="" class="responsive-img" style="height: 300px">--}}
 </div>
