672dbf40cc3c09dea59b6a02192b63c1b5137278
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2016-10-27 09:43:12 -0200

    Update layout.blade.php

--- app/Units/Authentication/Resources/Views/layout.blade.php
+++ app/Units/Authentication/Resources/Views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="@douglaszuqueto">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon.ico')}}">
     <meta name="apple-mobile-web-app-title" content="@douglaszuqueto">
     <meta name="application-name" content="@douglaszuqueto">
 
