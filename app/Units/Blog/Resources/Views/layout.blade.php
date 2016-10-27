e6a7d710b8e36a341c220fa50073820effa0b44c
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-27 19:35:07 -0200

    Layout

--- app/Units/Blog/Resources/Views/layout.blade.php
+++ app/Units/Blog/Resources/Views/layout.blade.php
@@ -62,17 +62,17 @@
 
 
 </script>
-<div id="fb-root"></div>
-<script>(function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.8&appId=191652421276345";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+{{--<div id="fb-root"></div>--}}
+{{--<script>(function (d, s, id) {--}}
+        {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
+        {{--if (d.getElementById(id)) return;--}}
+        {{--js = d.createElement(s);--}}
+        {{--js.id = id;--}}
+        {{--js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.8&appId=191652421276345";--}}
+        {{--fjs.parentNode.insertBefore(js, fjs);--}}
+    {{--}(document, 'script', 'facebook-jssdk'));</script>--}}
 
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5808c7bd4956dafc"></script>
+{{--<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5808c7bd4956dafc"></script>--}}
 </body>
 </html>
