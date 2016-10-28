b187c699ecc56e67acbfe5dab9dd33d104c2997e
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-10-27 22:25:49 -0200

    Layout

--- app/Units/Blog/Resources/Views/articles/index.blade.php
+++ app/Units/Blog/Resources/Views/articles/index.blade.php
@@ -1,59 +1,54 @@
 @extends('blog::layout')
 
 @section('content')
-    <div class="container index">
+    <div class="container articles">
         <div class="row">
-            <div class="col s12 l10">
+            <div class="col s12 l8">
+                <h1>Artigos</h1>
 
                 @foreach($articles as $article)
-                    <article class="col s12 l9 card">
-                        <div class="card-content black-text">
+                    <article class="article">
+                        <div class="col s12">
                             <div class="row article-header">
-                                <div class="col s12 l12">
-                                    <a href="{{$article->url}}" class="card-title black-text" style="font-weight: bold; font-size: 25px">
-                                        <span>{{$article->title}}</span>
-                                    </a>
+                                <div class="col s12">
+                                    <h2>
+                                        <a href="{{$article->url}}" class="card-title"> {{$article->title}} </a>
+                                    </h2>
                                 </div>
-                            </div>
-                            <div class="row article-image">
-                                <div class="col s12 l12">
+                                <div class="col s12">
                                     <a href="{{$article->url}}">
                                         <img src="{{$article->image}}" class="responsive-img">
                                     </a>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="card-content">
-                            <div class="row article-body">
-                                <div class="col l12">
+                                <div class="col s12">
                                     <p>{{$article->subtitle}}</p>
                                 </div>
                             </div>
-
-                        </div>
-
-                        <div class="card-content" style="border-top: 1px solid rgba(160,160,160,0.2);">
-                            <div class="article-footer">
-                                <div class="chip">
-                                    <a href="" class="black-text">ESP8266</a>
+                            <div class="col s12">
+                                <div class="row right ">
+                                    <div class="chip grey lighten-3">
+                                        <a href="" class="blue-text">ESP8266</a>
+                                    </div>
+                                    <div class="chip grey lighten-3">
+                                        <a href="" class="blue-text">IoT</a>
+                                    </div>
+                                    <div class="chip grey lighten-3">
+                                        <a href="" class="blue-text">MQTT</a>
+                                    </div>
                                 </div>
-                                <div class="chip">
-                                    <a href="" class="black-text">IoT</a>
-                                </div>
-                                <div class="chip">
-                                    <a href="" class="black-text">MQTT</a>
+                            </div>
+                            <div class="col s12">
+                                <div class="row">
+                                    <span class="author">Por <strong>Douglas Zuqueto</strong></span>
+                                    <span class="">{{$article->created_at}}</span>
                                 </div>
                             </div>
                         </div>
-                        <div class="col s12 l12">
-                            <div class="row">
-                                <span class="author">Por <strong>Douglas Zuqueto</strong></span>
-                                <span class="right">{{$article->created_at}}</span>
-                            </div>
-                        </div>
-
                     </article>
                 @endforeach
+
+            </div>
+            <div class="col l2">
 
             </div>
         </div>
