b00ac03958cfb85e5132e386a9afaec8c51c2b1e
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-11-02 10:53:39 -0200

    Layout - Artigos

--- app/Units/Blog/Resources/Views/articles/index.blade.php
+++ app/Units/Blog/Resources/Views/articles/index.blade.php
@@ -3,46 +3,46 @@
 @section('content')
     <div class="container articles">
         <div class="row">
-            <div class="col s12 l12">
-                <h1 class="center">Artigos</h1>
+            <div class="col s12 l10 offset-l1">
+                <h1>Artigos</h1>
 
                 @foreach($articles as $article)
                     <article class="article">
-                        <div class="col s8 offset-s2">
+                        <div class="col s10 offset-s1">
                             <div class="row article-header">
                                 <div class="col s12">
                                     <h2>
                                         <a href="{{$article->url}}" class="card-title"> {{$article->title}} </a>
                                     </h2>
                                 </div>
-                                <div class="col s12">
+                                <div class="col s12 center">
                                     <a href="{{$article->url}}">
                                         <img src="{{$article->image}}" class="responsive-img">
                                     </a>
                                 </div>
-                                <div class="col s12">
+                                <div class="col s12 subtitle">
                                     <p>{{$article->subtitle}}</p>
                                 </div>
                             </div>
-                            <div class="col s12">
-                                <div class="row right ">
-                                    <div class="chip grey lighten-3">
-                                        <a href="" class="blue-text">ESP8266</a>
-                                    </div>
-                                    <div class="chip grey lighten-3">
-                                        <a href="" class="blue-text">IoT</a>
-                                    </div>
-                                    <div class="chip grey lighten-3">
-                                        <a href="" class="blue-text">MQTT</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col s12">
-                                <div class="row">
-                                    <span class="author">Por <strong>Douglas Zuqueto</strong></span>
-                                    <span class="">{{$article->created_at}}</span>
-                                </div>
-                            </div>
+                            {{--<div class="col s12">--}}
+                                {{--<div class="row right ">--}}
+                                    {{--<div class="chip grey lighten-3">--}}
+                                        {{--<a href="" class="blue-text">ESP8266</a>--}}
+                                    {{--</div>--}}
+                                    {{--<div class="chip grey lighten-3">--}}
+                                        {{--<a href="" class="blue-text">IoT</a>--}}
+                                    {{--</div>--}}
+                                    {{--<div class="chip grey lighten-3">--}}
+                                        {{--<a href="" class="blue-text">MQTT</a>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<div class="col s12">--}}
+                                {{--<div class="row">--}}
+                                    {{--<span class="author">Por <strong>Douglas Zuqueto</strong></span>--}}
+                                    {{--<span class="">{{$article->created_at}}</span>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
                         </div>
                     </article>
                 @endforeach
