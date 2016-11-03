f9f71a9bd0b927be4e3e86d966f1f9f447fdb4e7
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-11-03 19:00:07 -0200

    Google Analytics

--- app/Units/Blog/Resources/Views/layout.blade.php
+++ app/Units/Blog/Resources/Views/layout.blade.php
@@ -73,6 +73,24 @@
 </script>
 <div id="fb-root"></div>
 @if(env('APP_ENV') == 'production')
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                        (i[r].q = i[r].q || []).push(arguments)
+                    }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                    m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-73205063-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
     <script>(function (d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) return;
