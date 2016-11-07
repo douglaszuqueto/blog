109ce363508ee34225158eb3fa3834a7fcef03c5
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-11-07 20:15:28 -0200

    Artigo: Upload de Imagens

---
+++ app/Domains/Articles/Repositories/ArticlesImagesRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Domains\Articles\Repositories;
+
+use App\Domains\Articles\Entities\ArticlesImages;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class ArticlesImagesRepository extends BaseRepository
+{
+
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return ArticlesImages::class;
+    }
+
+}
