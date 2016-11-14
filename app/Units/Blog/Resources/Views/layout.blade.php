c0193fea82efd6e234ce36f38063f0af7694c9eb
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-11-14 18:52:16 -0200

    Add HotJar

--- app/Units/Blog/Resources/Views/layout.blade.php
+++ app/Units/Blog/Resources/Views/layout.blade.php
@@ -110,5 +110,22 @@
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5808c7bd4956dafc"></script>
 @endif
 
+@if(env('APP_HOTJAR'))
+    <!-- Hotjar Tracking Code for https://douglaszuqueto.com/ -->
+    <script>
+        (function (h, o, t, j, a, r) {
+            h.hj = h.hj || function () {
+                        (h.hj.q = h.hj.q || []).push(arguments)
+                    };
+            h._hjSettings = {hjid: 335629, hjsv: 5};
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
+    </script>
+@endif
+
 </body>
 </html>
