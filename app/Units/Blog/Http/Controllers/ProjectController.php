689d2f7e62fccf4c73df74e8fd9186433022d6bc
douglaszuqueto <douglas.zuqueto@gmail.com>
2016-11-14 23:14:17 -0200

    SEO - Projeto

--- app/Units/Blog/Http/Controllers/ProjectController.php
+++ app/Units/Blog/Http/Controllers/ProjectController.php
@@ -11,11 +11,11 @@ class ProjectController extends Controller
 
     public function project()
     {
-        SEOMeta::setTitle('Venha conehcer o projeto Douglas Zuqueto');
+        SEOMeta::setTitle('Venha conhecer o projeto Douglas Zuqueto');
         SEOMeta::setDescription('Este projeto tem como objetivo disseminar conhecimento através de Artigos, Notícias e Videos abordando conteúdos sobre Hardware, Software e Plataformas para IoT');
         SEOMeta::setCanonical('https://douglaszuqueto.com/o-projeto');
 
-        OpenGraph::setTitle('Venha conehcer o projeto Douglas Zuqueto');
+        OpenGraph::setTitle('Venha conhecer o projeto Douglas Zuqueto');
         OpenGraph::setDescription('Este projeto tem como objetivo disseminar conhecimento através de Artigos, Notícias e Videos abordando conteúdos sobre Hardware, Software e Plataformas para IoT');
         OpenGraph::setUrl('https://douglaszuqueto.com/o-projeto');
         OpenGraph::addProperty('type', 'website');
